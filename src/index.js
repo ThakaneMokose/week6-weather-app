a8f561261f5a207be1300ca4aea2eaaa66792c14
ThakaneMokose <thakanemokose97@gmail.com>
2024-05-07 16:59:56 +0200

    improved the javascript

--- src/index.js
+++ src/index.js
@@ -61,4 +61,11 @@ function formatDate(date) {
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
+//let searchInputElement = document.querySelector("#search-input");
+let city = "Paris";
+
+let apiKey = "b2a5adcct04b33178913oc335f405433";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
 
